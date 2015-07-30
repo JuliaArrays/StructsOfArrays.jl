f65d99dbc3e95d1fe1f59ba664cf70a63d5c658f
Simon Kornblith <simon@simonster.com>
2015-07-29 20:12:34 -0400

    Better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using SoArrays
 using Base.Test
 
-regular = complex(randn(10000000), randn(10000000))
+regular = complex(randn(10000), randn(10000))
 soa = SoArray(Complex128, size(regular))
 copy!(soa, regular)
-@test_approx_eq regular soa
+@test_approx_eq sum(regular) sum(soa)
